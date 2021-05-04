993f46eb0911932f89102dd2439746d7dc6b0fc8
Brian Crafton <bcrafton3@gatech.edu>
2021-05-04 17:26:16 -0400

    step

--- train.py
+++ train.py
@@ -13,6 +13,8 @@ for device in gpu_devices:
 ####################################
 
 (x_train, y_train), (x_test, y_test) = load()
+x_train = x_train.astype(np.float32)
+y_train = y_train.astype(np.int64)
 
 ####################################
 
@@ -55,31 +57,41 @@ def gradients(model, x, y):
         pred_logits = model.train(x)
         pred_label = tf.argmax(pred_logits, axis=-1)
         loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=pred_logits)
-        correct = tf.reduce_sum(tf.cast(tf.equal(pred_label, y), tf.float32))
-        # total = tf.math.reduce_prod(tf.shape(y))
-        # acc = correct / total
-    
     grad = tape.gradient(loss, params)
-    return loss, correct, grad
+    return loss, grad, pred_label
+
+####################################
+
+def eval(labels, preds):
+    assert (np.shape(labels) == np.shape(preds))
+    assert (len(np.shape(labels)) == 4)
+    correct = (labels == preds) * labels
+    correct = np.sum(correct, axis=(0,1,2))
+    total = np.sum(labels, axis=(0,1,2))
+    assert (np.all(correct <= total))
+    return correct, total
 
 ####################################
 
 batch_size = 5
 for _ in range(10):
-    total_correct = 0
+    _correct, _total = 0, 0
     for batch in range(0, len(x_train), batch_size):
-        xs = x_train[batch:batch+batch_size].astype(np.float32)
-        ys = y_train[batch:batch+batch_size].astype(np.int64)
-        loss, correct, grad = gradients(model, xs, ys)
+        xs = x_train[batch:batch+batch_size]
+        ys = y_train[batch:batch+batch_size]
+
+        loss, grad, pred = gradients(model, xs, ys)
         optimizer.apply_gradients(zip(grad, params))
-        total_correct += correct
 
-        # out = pred(model, xs)
-        # tf.print(tf.shape(out))
-        # tf.print(tf.shape(xs))
-        # tf.print(tf.shape(ys))
-
-    print (total_correct / len(x_train) / (512 * 512) * 100)
+        label_np = tf.one_hot(ys, depth=4, axis=-1).numpy()
+        pred_np = tf.one_hot(pred, depth=4, axis=-1).numpy()
+        correct, total = eval(labels=label_np, preds=pred_np)
+        
+        _correct += correct
+        _total += total
+        
+    accuracy = _correct / _total * 100
+    print(accuracy)
 
 ####################################
 
